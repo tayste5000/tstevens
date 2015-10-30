29e37b06e143a66d440827463601c6d1455d9fba
unknown <tayste5000@gmail.com>
2015-10-30 14:46:05 -0400

    structures

--- public/js/structures.js
+++ public/js/structures.js
@@ -286,11 +286,11 @@ return {
         else {
           viewer.hide("*")
           viewer.ballsAndSticks("lysozyme", structures["lysozyme"].select({"rnumRange":[1,count+1]}),{radius: 0.2});
-          count += 5;
+          count += 10;
           lysozymeLoop();
         }
 
-      },200)
+      },250)
     }
   },
   "estrogen": {
